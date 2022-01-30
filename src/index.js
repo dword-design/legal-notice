01d416706428ac9373b84c62957e09a771cc89c3
dword <info@sebastianlandwehr.com>
2022-01-30 15:35:48 +0000

    feat: init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { endent } from '@dword-design/functions'
+
+export default endent`
+  <h1>Legal Notice</h1>
+  <p>
+    Sebastian Landwehr<br>
+    Breite Str. 20<br>
+    53111 Bonn
+  </p>
+
+  <h2>Contact</h2>
+  <p>
+    E-Mail: <a href="mailto:info@sebastianlandwehr.com">info@sebastianlandwehr.com</a><br>
+    Phone: +49 177 5055641<br>
+  </p>
+
+  <h2>Person responsible for content in accordance with 55 Abs. 2 RStV</h2>
+  <p>
+    Sebastian Landwehr<br>
+    Breite Str. 20<br>
+    53111 Bonn
+  </p>
+`
