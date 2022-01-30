ee089bb1d82c53ae6359f324c6cf0571acffa2c7
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-01-30 16:50:28 +0100

    fix: i18n

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { endent } from '@dword-design/functions'
 
-export default endent`
+export const en = endent`
   <h1>Legal Notice</h1>
   <p>
     Sebastian Landwehr<br>
@@ -10,11 +10,33 @@ export default endent`
 
   <h2>Contact</h2>
   <p>
+    Email: <a href="mailto:info@sebastianlandwehr.com">info@sebastianlandwehr.com</a><br>
+    Phone: +49 177 5055641<br>
+  </p>
+
+  <h2>Person responsible for content in accordance with § 55 Abs. 2 RStV</h2>
+  <p>
+    Sebastian Landwehr<br>
+    Breite Str. 20<br>
+    53111 Bonn
+  </p>
+`
+
+export const de = endent`
+  <h1>Impressum</h1>
+  <p>
+    Sebastian Landwehr<br>
+    Breite Str. 20<br>
+    53111 Bonn
+  </p>
+
+  <h2>Kontakt</h2>
+  <p>
     E-Mail: <a href="mailto:info@sebastianlandwehr.com">info@sebastianlandwehr.com</a><br>
     Phone: +49 177 5055641<br>
   </p>
 
-  <h2>Person responsible for content in accordance with 55 Abs. 2 RStV</h2>
+  <h2>Redaktionell verantwortlich gemäß § 55 Abs. 2 RStV</h2>
   <p>
     Sebastian Landwehr<br>
     Breite Str. 20<br>
